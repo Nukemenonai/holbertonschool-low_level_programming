cd56ef517707a286dcd8b9a105f9ee4456599791
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-26 17:31:27 -0500

    add: NULL separator condition

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -23,10 +23,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		for (i = 0; i < n ; i++)
 		{
 			value = va_arg(valist, int);
-			if (i == n - 1)
-				printf("%d", value);
-			else
-				printf("%d%s", value, separator);
+			printf("%d", value);
+
+			if (i < n - 1 && separator != NULL)
+				printf("%s", separator);
 		}
 		printf("\n");
 
