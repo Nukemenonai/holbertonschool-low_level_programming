340a800c54cb96e1e63231b64152bb52286996d6
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-26 16:42:33 -0500

    fix: structure params

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,20 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+/**
+ *
+ *
+ *
+ */
+
+typedef struct select
+{
+	char *c;
+	void(*f)(va_list valist);
+} my_sel;
+
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
