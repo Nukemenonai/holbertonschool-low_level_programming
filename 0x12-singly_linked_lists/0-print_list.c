065d840e74098ef557fa2686ac7feb28047f290f
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-31 15:18:17 -0500

    fix: initialize len in 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,5 @@
 #include "lists.h"
 
-
 /**
  * print_list - prints all the elements of a linked list
  *
@@ -10,7 +9,7 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t len;
+	size_t len = 0;
 
 	while (h != NULL)
 	{
@@ -18,8 +17,8 @@ size_t print_list(const list_t *h)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-		len++;
 		h = h->next;
+		len++;
 	}
 
 	return (len);
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -11,7 +11,7 @@
 
 size_t list_len(const list_t *h)
 {
-	size_t len;
+	size_t len = 0; 
 
 	while (h != NULL)
 	{
