a80ebe6eece7c24cf79cf151d23cf4e00fbbff63
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-31 14:57:57 -0500

    fix: betty format

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,7 +14,7 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		if(h->str == NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 		{
@@ -26,5 +26,5 @@ size_t print_list(const list_t *h)
 		h = h->next;
 	}
 
-	return(len);
+	return (len);
 }
--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
+ * add_node - adds a new note to a linked list
  *
+ * @head: the pointer of the first node
  *
- *
- *
- *
- *
+ * @str: the string to pupulate in the new node
+ * Return: the pointer to the new node
  */
 
 list_t *add_node(list_t **head, const char *str)
@@ -15,18 +15,24 @@ list_t *add_node(list_t **head, const char *str)
 	char *s = strdup(str);
 	unsigned int mylen;
 
+	if (str == NULL)
+		return (NULL);
+
 	for (len = 0; s[len] != '\0'; len++)
 	{}
 
 	if (new_node == NULL)
-		return NULL;
+		return (NULL);
 	/* creating the node using malloc */
 
 
 	new_node->len = mylen;
 	new_node->str = s;
-	new_node->next = *head;
-	*head = new_node;
+	if (head != NULL)
+	{
+		new_node->next = *head;
+		*head = new_node;
+	}
 
 	/* populating the new node  */
 
