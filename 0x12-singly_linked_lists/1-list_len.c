2ab9dde3287b068e72aa239cfc7c6ea43841a3a1
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-31 19:38:44 -0500

    fix: betty format

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,14 +4,12 @@
  * list_len - returs the length of a list
  *
  * @h: pointer to the next element
- *
  * Return: len of the list
- *
  */
 
 size_t list_len(const list_t *h)
 {
-	size_t len = 0; 
+	size_t len = 0;
 
 	while (h != NULL)
 	{
