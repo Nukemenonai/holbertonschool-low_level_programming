1aa2376946dcdf77a33313316b8a5337dbe79b0c
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-11-04 17:39:51 -0500

    fix: invalid read

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,12 +9,15 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
+	listint_t *next;
+
 	temp = *head;
 
 	while (temp != NULL)
 	{
+		next = temp->next;
 		free(temp);
-		temp = *head->next;
+		temp = next;
 	}
 
 	*head = NULL;
