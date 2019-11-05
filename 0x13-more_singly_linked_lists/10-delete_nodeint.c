279bea0a38980b55251438c94a3976fe4b5fa920
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-11-05 16:50:18 -0500

    fix: double initialization

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	listint_t *temp = *head;
 	listint_t *next;
-	unsigned int count = 0;
+	unsigned int count;
 
 	if (*head == NULL)
 		return (-1);
