24d1787473b1ad36f1542113e089b17deafacdf6
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-11-05 17:36:30 -0500

    temp = head at beggining of else

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,6 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int count = 0;
 
 	new_node = malloc(sizeof(listint_t));
+
 	if (new_node == NULL)
 		return (NULL);
 
@@ -28,6 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	else
 	{
+		temp = *head;
 		while (count < (idx - 1))
 		{
 			count++;
@@ -40,7 +42,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		new_node->next = temp->next;
 		temp->next = new_node;
-		return (temp);
+		return (new_node);
 
 	}
 	return (new_node);
