25d7dd956b9867fefdd0a0b4d966f8f195ff1c13
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-11-05 16:55:37 -0500

    fix: order of elements inside while

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	int len;
+	int len = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		h = h->next;
 		len++;
+		h = h->next;
 
 	}
 	return (len);
