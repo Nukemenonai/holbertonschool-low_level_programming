f8d4df80d792dbb6503d35b01036755bdc722787
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-11-05 12:06:08 -0500

    fix return head pointer

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,10 +12,9 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node;
-	listint_t *temp;
+	listint_t *temp = *head;
 	unsigned int count = 0;
 
-	temp = *head;
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
@@ -30,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			new_node->next = temp->next;
 			temp->next = new_node;
-			return (temp);
+			return (*head);
 
 		}
 	}
