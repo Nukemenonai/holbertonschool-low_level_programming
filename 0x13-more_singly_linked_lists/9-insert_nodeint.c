3ad81dc57a944e71f059b3fc9aa2227978e3e658
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-11-05 11:15:55 -0500

    fix: logic order and exit condition

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		count++;
 		temp = temp->next;
-		if (count == idx)
+		if (count == idx - 1)
 		{
 			new_node->next = temp->next;
 			temp->next = new_node;
