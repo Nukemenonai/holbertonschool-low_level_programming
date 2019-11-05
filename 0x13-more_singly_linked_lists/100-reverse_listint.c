d39e36f48c19fde6eef07fcd26e77d5210622791
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-11-05 18:47:51 -0500

    fix: betty format

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,9 +1,10 @@
 #include "lists.h"
 
 /**
+ * reverse_listint - reverses a linked list
  *
- *
- *
+ * @head: the pointer to the first index
+ * Return: the pointer to the new list head
  */
 
 listint_t *reverse_listint(listint_t **head)
