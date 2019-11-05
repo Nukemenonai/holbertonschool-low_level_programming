74122b0359b947bd6d7122e34575bce4eda063bb
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-11-05 08:59:55 -0500

    add task #4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a linked list of an int
+ *
+ * @head: the pointer to the first element of the list
+ */
+
+void free_listint(listint_t *head)
+{
+	if (head != NULL)
+	{
+		free_listint(head->next);
+		free(head);
+	}
+
+}
