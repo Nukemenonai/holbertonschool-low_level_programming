1b6493bec3177feb4ce55e793a9adebb1752a22b
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-11-04 19:11:07 -0500

    fix: betty format

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,10 +1,11 @@
 #include "lists.h"
 
 /**
+ * get_nodeint_at_index - gets the node at Nth position
  *
- *
- *
- *
+ * @head: the pointer to the first node
+ * @index: the node to look for
+ * Return: the pointer if index is found or NULL if not
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
