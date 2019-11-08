29a47428558162e74f17853c816876a5eef00cee
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-11-07 19:01:20 -0500

    add: task 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int i;
+
+	if (index > 64)
+		return (-1);
+
+	for (i = 0; i < index; i++)
+	{
+	        *n = (*n | 1 << index);
+
+	}
+	return (*n);
+
+}
