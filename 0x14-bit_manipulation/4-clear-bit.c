91b999b77fdf4c360d9d16c878a18a7b352af179
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-11-07 19:25:23 -0500

    add: task #4

---
+++ 0x14-bit_manipulation/4-clear-bit.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * clear_bit - clears the bit at n position
+ *
+ * @n: number to edit
+ * @index: the position to edit at binary level
+ * Return: 1 if successful, -1 otherwise
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index);
+
+{
+	unsigned int i;
+
+	if (index > 64)
+		return (-1);
+
+	for (i = 0; i <= index; i++)
+	{
+		*n = (*n & (~1 << index));
+
+	}
+	return (1);
+
+}
