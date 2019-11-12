69325e033322a5ad0bbc008e9b5a4cc4242e1a54
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-11-12 16:55:44 -0500

    fix: betty format

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -38,7 +38,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		if(filename != NULL)
+		if (filename != NULL)
 			return (1);
 		else
 			return (-1);
