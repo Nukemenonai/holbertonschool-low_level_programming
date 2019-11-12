32d35a98cda7c504bab2866b0663333b5926019d
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-11-12 14:35:42 -0500

    fix: betty format

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,10 +1,11 @@
 #include "holberton.h"
 
 /**
+ * read_textfile - prints a text to the posix stdout
  *
- *
- *
- *
+ * @filename: the name of the file to print
+ * @letters: the number of letters to print
+ * Return: the number of characters or 0 if the process fails. 
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
@@ -27,6 +28,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 
 	j = write(1, buff, i);
+
+	if (j == -1)
+		return (0);
+
 	return (j);
 
 }
