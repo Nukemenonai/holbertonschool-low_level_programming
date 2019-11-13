8fd3b38c74d221cf84c10e4db6c05d87c918a602
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-11-12 21:29:17 -0500

    fix: other stuff

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -27,12 +27,15 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
 			av[1]), exit(98);
 	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
+			av[1]), exit(99);
 	while ((i = read(fd, buff, 1024)) > 0)
 	{
 		j = write(fd2, buff, i);
 		if (i != j)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n"
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n"
 				, av[2]), exit(99);
 		}
 	}
@@ -40,10 +43,6 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
 			av[1]), exit(98);
 
-	if (j == -1)
-
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
-			av[2]), exit(99);
 	k = close(fd);
 	l = close(fd2);
 	if (k == -1 || l == -1)
