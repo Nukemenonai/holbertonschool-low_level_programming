fd0f478a9356a0bfc633669aa95a06b3af8b5e33
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-11-12 19:23:52 -0500

    add task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,76 @@
+#include "holberton.h"
+
+/**
+ * _cp - copy the contents of one file to another
+ *
+ * @file_from: the file to copy from
+ * @file_to: the file to copy to
+ *
+ */
+
+void _cp(char *file_from, char *file_to)
+{
+	int fd, fd2;
+	int i, j, k, l;
+	char *buff = malloc(1024);
+
+	fd = open(file_from, O_RDONLY, 0664);
+
+	if (fd == -1)
+	{
+
+		printf("Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	i = read(fd, buff, 1024);
+
+	if (i == -1)
+	{
+		printf("Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	fd2 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	j = write(fd2, buff, 1024);
+
+	if (j == -1)
+	{
+		printf("Error: Can't write to %s\n", file_to);
+	}
+
+	k = close(fd);
+	l = close(fd2);
+
+	if (k == -1 || l == -1)
+	{
+		printf("Error: Can't close fd %d\n", k);
+		exit(100);
+	}
+}
+
+/**
+ * main - checks my code :)
+ *
+ * @ac: argument count
+ * @av: argument vector
+ * Return: Always 0
+ */
+
+
+int main(int ac, char **av)
+{
+
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to %s\n", av[0]);
+		exit(97);
+	}
+
+
+	_cp(av[1], av[2]);
+
+	return (0);
+}
