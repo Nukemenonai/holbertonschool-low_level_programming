cc199e06d3f4b4418026682daa08e7ebf101a7fc
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-11-12 21:13:56 -0500

    fix: something

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,17 +20,13 @@ int main(int ac, char **av)
 	char buff[1024];
 
 	if (ac != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
+			exit(97);
 	fd = open(av[1], O_RDONLY);
 	if (fd == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
 			av[1]), exit(98);
-
 	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd2 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
-			av[1]), exit(98);
 	while ((i = read(fd, buff, 1024)) > 0)
 	{
 		j = write(fd2, buff, i);
