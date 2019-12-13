319cccd8617e950977ead95f8f001909f7f33038
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-13 11:06:05 -0500

    [add]: files for task 1 - list length

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - returns the size of a linked list.
+ *
+ * Return: the number of nodes
+ */
+
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t size = 0;
+	const dlistint_t *mv = h;
+
+	while (mv->next != NULL)
+	{
+		mv = mv->next;
+		size++;
+	}
+	size++;
+
+	return (size);
+
+}
