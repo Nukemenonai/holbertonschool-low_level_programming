c72612a3b967f10f78c7a3a3ed46796dcfba49d8
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-13 17:47:03 -0500

    [ADD]: files for task 6 sum

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
