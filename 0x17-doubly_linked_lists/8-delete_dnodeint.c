c8f84673203d587c8a7962e9d24a0fd56a71a162
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-14 13:18:52 -0500

    [ADD] files for task 8 delete node at n pos

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - delets node at nth pos of a dllist.
+ *
+ * @head: 
+ * Return: 1 if successful  -1 if error
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp = *head;
+	dlistint_t *desps;
+	unsigned int count;
+
+	if (*head == NULL || (*head)->next == NULL)
+		return (-1);
+
+	else if (index == 0)
+	{
+		*head = temp->next;
+		temp->prev = NULL;
+		free(temp);
+		return (1);
+	}
+	else
+	{
+		for (count = 0; count < index - 1 && temp != NULL; count++)
+			temp = temp->next;
+
+		desps = temp->next->next;
+		free(temp->next);
+		temp->next = desps;
+		desps->prev = temp;
+		return (1);
+
+	}
+	return (-1);
+}
