be223b8336c8efce34c0949790db6b84100a22fe
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-14 13:29:52 -0500

    fix: betty format

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -16,7 +16,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *last = *head;
 
 	if (new == NULL)
-		return NULL;
+		return (NULL);
 	new->n = n;
 	new->next = NULL;
 	if (*head == NULL)
