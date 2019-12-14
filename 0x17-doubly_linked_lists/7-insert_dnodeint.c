5a6a369121e32bc26321ddbdf2816235349ddf14
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-14 15:36:58 -0500

    fix: tipo

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -20,7 +20,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *tmp = *h;
 	dlistint_t *repl;
 
-	if (*h == NULL || idx < 0)
+	if (*h == NULL)
 		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
