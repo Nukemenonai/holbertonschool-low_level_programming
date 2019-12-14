5204f103d1521cf528c53a2fe4a2faad5fb4714c
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-14 14:38:48 -0500

    FIX: added previous link to next node after new

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -44,6 +44,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new->next = tmp->next;
 		new->prev = tmp;
 		tmp->next = new;
+		tmp->next->prev = new;
 		return (new);
 	}
 	return (new);
