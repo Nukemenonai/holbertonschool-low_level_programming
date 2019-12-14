8863d88be9407e71a3843c933b58c41857b3ea64
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-14 17:00:46 -0500

    yet another additional check for idx 0

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -29,7 +29,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			new->next = *h;
 			(*h)->prev = new;
 		}
-		*h = new;
+		*h = new, new->next = NULL;
 		return (new);
 	}
 	else
