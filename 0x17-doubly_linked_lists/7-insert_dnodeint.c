dd36f1c40e01abe69832499ecfcfb33fff07c1a1
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-14 13:43:32 -0500

    fix: betty format

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -4,8 +4,11 @@
 #include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
+ * insert_dnodeint_at_index - inserts a node at a given position
  *
+ * @h: the pointer to the first node of the list
+ * idx: the index to get to
+ * @n: the data to populate data field
  * Return: Always EXIT_SUCCESS.
  */
 
@@ -14,6 +17,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int pos = 0;
 	dlistint_t *new;
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
@@ -27,7 +31,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	else
 	{
-		while (pos < (idx -1))
+		while (pos < (idx - 1))
 		{
 			*h = (*h)->next;
 			if (*h == NULL)
