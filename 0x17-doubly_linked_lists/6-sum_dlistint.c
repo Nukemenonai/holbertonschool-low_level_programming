b306f525217646ce8a7351aeb5098ceea30189fe
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-12-14 13:39:59 -0500

    fix: betty format

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -4,14 +4,16 @@
 #include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
+ * sum_dlistint - sums the data in the nodes of a linked list
  *
+ * @head: the pointer to the first node
  * Return: Always EXIT_SUCCESS.
  */
 
 int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
+
 	while (head != NULL)
 	{
 		sum += head->n;
