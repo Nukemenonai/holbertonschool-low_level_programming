73daf7fd1e2512a14e7e809f85981dd47cda58fb
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-01-18 20:04:25 +0000

    fix: betty format

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,19 +10,19 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-        hash_table_t *new = malloc(sizeof(hash_table_t));
+	hash_table_t *new = malloc(sizeof(hash_table_t));
 
 	if (!new)
 		return NULL;
 
-        new->size = size;
-        new->array = calloc(size, sizeof(hash_node_t *));
+	new->size = size;
+	new->array = calloc(size, sizeof(hash_node_t *));
 
-        if (new->array == NULL)
+	if (new->array == NULL)
 	{
 		free(new);
 		return NULL;
 	}
 	else
-                return (new);
+		return (new);
 }
