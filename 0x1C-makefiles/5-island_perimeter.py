303832a1f5a2c6ec4e111918b9562782e1fe8315
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-02-22 01:09:59 +0000

    [fix] checking additional condition for tests of land

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,13 +8,13 @@ def island_perimeter(grid):
     pos = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            if grid[i][j] == 1:
-                if grid[i - 1][j] != 1:
+            if grid[i][j]:
+                if not j or not grid[i][j-1]:
                     perimeter += 1
-                if grid[i + 1][j] != 1:
+                if not i or not grid[i-1][j]:
                     perimeter += 1
-                if grid[i][j + 1] != 1:
+                if j == n - 1 or not grid[i][j+1]:
                     perimeter += 1
-                if grid[i][j - 1] != 1:
+                if i == m - 1 or not grid[i+1][j]:
                     perimeter += 1
     return perimeter
