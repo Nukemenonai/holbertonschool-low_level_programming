46bbc14a395ae5a45d41d459270f7c851fb714f6
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-04-15 17:17:37 +0000

    modified exit condition

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -17,7 +17,7 @@ int linear_search(int *array, size_t size, int value)
 
 	if (!value || !array)
 		return (-1);
-	for (i = 0; i <= size; i++)
+	for (i = 0; i <= size - 1; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
