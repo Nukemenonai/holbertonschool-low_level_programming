d33efe93aef629c4c1fbe75e4362ef7760535132
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-04-15 17:09:20 +0000

    [fix]: added prototypes for task 1

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,5 +5,7 @@
 /*Prototypes*/
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void print_array(int *array, int low, int high);
 
 #endif
